0bcf0daa96166a7c6e8f893efa86a1a633e98549
Prattay01 <prattay846@gmail.com>
2026-06-03 16:16:23 +0530

    Add Day 02 Question 3 data types exercise

---
+++ Corporates_Guide_Heritage_PythonDsa/Day 02/q3.py
@@ -0,0 +1,11 @@
+employee_count = 15
+discount_percentage = 12.5
+store_location = "Kolkata"
+is_open = True
+special_remarks = None
+
+print("Value:", employee_count, "| Type:", type(employee_count))
+print("Value:", discount_percentage, "| Type:", type(discount_percentage))
+print("Value:", store_location, "| Type:", type(store_location))
+print("Value:", is_open, "| Type:", type(is_open))
+print("Value:", special_remarks, "| Type:", type(special_remarks))
